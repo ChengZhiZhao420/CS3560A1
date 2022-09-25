4bd3ef01fa0ea33f3f7f3ee004a269f516382b3e
Jared <JaredZ@unitedlegwear.com>
2022-09-24 20:45:37 -0700

    updated component for VotingService when user input his answer, it will update the statistic hashmap by using the answer as key to find the value, and increment that value by 1.

--- src/VotingService.java
+++ src/VotingService.java
@@ -8,22 +8,31 @@ public class VotingService implements Voting{
     private HashMap<Character, Integer> statistic;
 
     public VotingService(AbstractQuestion question){
-        students = new HashSet<>();
-        statistic = new HashMap<>();
         setComponent(question);
     }
 
     @Override
     public void setComponent(AbstractQuestion q) {
+        students = new HashSet<>();
+        statistic = new HashMap<>();
         this.question = q.getQuestion();
         this.answerSet = q.getAnswer();
+
+        answerSet.forEach((key, value)-> statistic.put(key, 0));
     }
 
     @Override
     public void startVoting(Student s, char answer) {
         String studentID = s.getID();
         if(!students.contains(studentID)){
-            students.add(studentID);
+            if(answerSet.containsKey(answer)){
+                students.add(studentID);
+                statistic.put(answer, statistic.get(answer) + 1);
+
+            }else{
+                System.out.println("Your answer is not in answer set");
+            }
+
         }else{
             System.out.println("You already answered");
         }
@@ -36,13 +45,11 @@ public class VotingService implements Voting{
 
     @Override
     public void displayAnswer() {
-        answerSet.forEach((k,v)->{
-            System.out.println(k + ". " + v);
-        });
+        answerSet.forEach((k,v)-> System.out.println(k + ". " + v));
     }
 
     @Override
     public void displayStatistics() {
-
+        statistic.forEach((key, value)-> System.out.println("Key: " + key + "; Number: " + value));
     }
 }
